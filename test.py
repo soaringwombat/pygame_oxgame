f00b084e017650fd303ec0bfb9622070736e1c2b
yosuke <yousuke.k2001@gmail.com>
2023-08-27 19:44:53 +0900

    :sparkles: [add]矢印キーによる移動機能を実装

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import QUIT, KEYDOWN, K_UP, K_DOWN, K_RIGHT, K_LEFT
 
 # ゲーム画面を初期化
 pygame.init()
@@ -21,8 +21,8 @@ px = 0  # プレイヤーの位置
 py = 0
 
 
-# マスの移動
-def move(key):
+# キー入力を処理
+def push_key(key):
     global px, py
     if key == K_UP:
         py = max(0, py - 1)
@@ -48,14 +48,9 @@ def reset_board():
 
 # メインループ
 def main():
+    global px, py
     global end_flag
     while True:
-        # イベントを処理
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-
         # 画面を背景色でクリア
         screen.fill(back)
 
@@ -66,8 +61,19 @@ def main():
         pygame.draw.line(screen, black, (175, 225), (625, 225), 5)
         pygame.draw.line(screen, black, (175, 375), (625, 375), 5)
 
+        # 選択中のマスを描画
+        pygame.draw.rect(screen, white,
+                         (175 + px * 150, 75 + py * 150, 150, 150), 5)
+
         # 画面を更新
         pygame.display.update()
+        # イベントを処理
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                push_key(event.key)
 
 
 if __name__ == '__main__':
